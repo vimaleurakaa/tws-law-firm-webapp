eddd50232b5103dccec5b780b1ea484e364e15f9
Vimal Thanikachalam <vimaleurakaa@gmail.com>
2021-03-25 15:05:23 +0530

    sass-install

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import "antd/dist/antd.css";
 import App from "./Containers/App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
